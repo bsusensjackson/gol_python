af7b84b4da1c885ba3a4f760f3141c55357c5101
Ben Brostoff <ben.brostoff@gmail.com>
2014-10-29 17:40:59 -0400

    Beginning game logic

--- terminal_app_gol/gol.py
+++ terminal_app_gol/gol.py
@@ -1,5 +1,24 @@
-class Cell():
-  def __init__(self, x, y, alive):
-    self.x = x
-    self.y = y
-    self.alive = alive 
+class Game():
+  def __init__(self, current): # array of arrays with 0/1
+    self.current = current
+    self.next = []
+
+  def find_neighbors(self, x, y): # needs to be refactored
+    return [self.current[x - 1][y - 1],
+            self.current[x - 1][y],
+            self.current[x - 1][y + 1],
+            self.current[x][y - 1],
+            self.current[x][y + 1],
+            self.current[x + 1][y - 1],
+            self.current[x + 1][y],
+            self.current[x + 1][y + 1]]
+
+
+# 5 x 5
+# (1, 2), (2,2), (3,2)
+# (2, 1), (2,2), (2,3)
+
+# Any live cell with fewer than two live neighbours dies, as if caused by under-population.
+# Any live cell with two or three live neighbours lives on to the next generation.
+# Any live cell with more than three live neighbours dies, as if by overcrowding.
+# Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
