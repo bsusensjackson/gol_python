291badfc74de731069215ba6730bd6167d2435b8
Ben Brostoff <ben.brostoff@gmail.com>
2014-10-30 22:20:24 -0400

    Make from top level directory

--- terminal_app_gol/play.py
+++ terminal_app_gol/play.py
@@ -1,17 +1,27 @@
-from gol import *
 import sys
+import random
+from gol import *
 
-def preface(file, folder='patterns/'):
+def preface(file, folder='terminal_app_gol/patterns/'):
   return folder + file
 
-options = {"Gosper": preface('gosper_glider_gun.txt'),
-           "Glider": preface('glider.txt'),
-           "Rand": 0 }
+options = { "gosper": preface('gosper_glider_gun.txt'),
+            "glider": preface('glider.txt') }
 
 def play():
-    if sys.argv[1] in options.keys():
-      game = Game(options[sys.argv[1]], 1)
+    u_input = sys.argv[1].lower()
+    if u_input in options.keys():
+      game = Game(options[u_input], 1)
       game.play() 
+    elif u_input == "rand":
+      x_bound = random.randint(15, 35)
+      y_bound = random.randint(15, 35)
+      game = Game(
+                   [[random.randint(0,1) for x in xrange(x_bound)] 
+                     for x in xrange(y_bound)]
+                  )
+      game.play()
+
     else:
       print "We don't have that pattern... yet."
 
